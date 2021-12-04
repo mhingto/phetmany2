2b6e26e3b866fe92d92ee3a9383414fb7e312a7a
mhingto <phetmany.mhing@gmail.com>
2021-12-04 07:27:05 +0700

    phetmay

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <h1>Fetch data with API</h1>
+        <EmployeeList />
+    </div>
+</template>
+<script>
+    import EmployeeList from './components/EmployeeList'
+    export default {
+        name:"App",
+        comments: {
+            EmployeeList
+        }
+    };
+</script>
+
+<style>
+    #app{
+        font-family: Phetsarath OT, Times New Roman;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Employee List</h1>
+    </div>
+</template>
+<script>
+    import Vue from 'vue';
+    import axios from 'axios'
+    import VueAxios from 'vue-axios'
+    Vue.use(VueAxios, axios)
+    export default {
+        name:"EmployeeList",
+        mounted() {
+            Vue.axios.get('http://dummy.restapiexample.com/api/v1/employees')
+            .then((resp)=>{
+                console.warn(resp)
+            })
+        },
+    }
+</script>
